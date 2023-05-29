833a25f323f094bc9e5174577df1a56285eabbe3
eyezah (via Perfect Girlfriend) <github@eyezah.com>
2023-05-29 13:57:50 +1200

    add new components

---
+++ src/components/room-popout-button.component.tsx
@@ -0,0 +1,25 @@
+import { IconContext } from "react-icons";
+import styles from "../styles/room-popout-button.component.module.scss";
+import { LuInstagram } from "react-icons/lu";
+
+export interface RoomPopoutButtonProps {
+    selected?: boolean
+    onclick?: () => void
+    children?: JSX.Element
+    open?: boolean
+}
+
+export default function RoomPopoutButton(props: RoomPopoutButtonProps) {
+    function click(e: React.MouseEvent) {
+        e.stopPropagation();
+        if (props.onclick) props.onclick();
+    }
+
+    return (
+        <button className={styles.button + (props.selected ? ` ${styles.selected}` : "") + (!props.children ? ` ${styles.arrow}` : "") + (props.open ? ` ${styles.open}` : "")} onClick={click}>
+            <IconContext.Provider value={{size: "40px"}}>
+                { props.children }
+            </IconContext.Provider>
+        </button>
+    )
+}
