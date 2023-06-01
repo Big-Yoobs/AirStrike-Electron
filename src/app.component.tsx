ac2616850eee44322cf5f4499ebd84e5e95d327b
Thomas Richards <106329898+tomphonse@users.noreply.github.com>
2023-06-01 16:16:29 +1200

    library styling

--- src/app.component.tsx
+++ src/app.component.tsx
@@ -7,7 +7,7 @@ export default function AppComponent() {
         <>
             <HeaderComponent />
             {/* <RoomComponent /> */}
-            {/* <LibraryComponent/> */}
+            <LibraryComponent/>
 
         </>
     )
