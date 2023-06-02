1c69df0ddf3ba5b7fc4107d61b1fea5f3474b1b1
Thomas Richards <106329898+tomphonse@users.noreply.github.com>
2023-06-02 16:08:30 +1200

    joinRoomModal done

--- src/app.component.tsx
+++ src/app.component.tsx
@@ -1,6 +1,7 @@
 import HeaderComponent from "./components/header-component";
 import LibraryComponent from "./components/library.component";
 import RoomComponent from "./components/room.component";
+import RoomModalComponent from "./components/room-modal.component";
 import { electron } from "./utils";
 
 electron().addEventListener("room ID", console.log);
@@ -9,6 +10,7 @@ export default function AppComponent() {
     return (
         <>
             <HeaderComponent />
+            <RoomModalComponent/>
             {/* <RoomComponent /> */}
             <LibraryComponent/>
         </>
