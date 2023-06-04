141426aa303965cafa43cea25870ea73a4f7d51f
Thomas Richards <106329898+tomphonse@users.noreply.github.com>
2023-06-05 10:36:54 +1200

    some messing around

---
+++ src/components/start-page.component.tsx
@@ -0,0 +1,33 @@
+import styles from "../styles/start-page.component.module.scss";
+
+import {BsFillNutFill} from "react-icons/bs";
+import { FaTheaterMasks } from "react-icons/fa";
+import {IoGameController} from "react-icons/io5";
+
+export default function StartPageComponent() {
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.logo}>
+                <img src="https://i.imgur.com/8otsuij.png" />
+            </div>
+
+            <div className={styles.buttons}>
+                <div className={styles.button}>
+                    <div className={styles.buttonIcon}><IoGameController/></div>
+                    <div className={styles.buttonText}>Games</div>
+                </div>
+                <div className={styles.button}>
+                    <div className={styles.buttonIcon}><FaTheaterMasks/></div>
+                    <div className={styles.buttonText}>Video</div>
+                </div>
+                <div className={styles.button}>
+                    <div className={styles.buttonIcon}><BsFillNutFill/></div>
+                    <div className={styles.buttonText}>Settings</div>
+                </div>
+            </div>
+
+        </div>
+
+    );
+}
